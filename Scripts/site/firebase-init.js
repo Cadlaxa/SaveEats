9cc76669889b20b826050df88999be980b2f9cd1
cadlaxa <cadag113003@gmail.com>
2026-01-08 21:56:17 +0800

    Revert

--- Scripts/site/firebase-init.js
+++ Scripts/site/firebase-init.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { 
-  initializeFirestore, 
-  persistentLocalCache, 
-  persistentMultipleTabManager 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"; 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 export const firebaseConfig = {
@@ -17,12 +13,6 @@ export const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-
-export const db = initializeFirestore(app, {
-  cache: persistentLocalCache({
-    tabManager: persistentMultipleTabManager()
-  })
-});
-
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
